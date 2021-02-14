657796fb759d22a0a6dadb68ad70076f02135e14
Florian de Boissieu <fdeboiss@gmail.com>
2021-02-14 03:18:16 +0100

    add entry point and add cli to docs with mkdocs-click

---
+++ docs/cli.md
@@ -0,0 +1,11 @@
+# CLI Reference
+
+This page provides documentation for our command line tools.
+
+::: mkdocs-click
+    :module: fordead.cli.__main__
+    :command: fordead
+    :depth: 1
+
+
+
--- setup.py
+++ setup.py
@@ -152,5 +152,7 @@ setup(
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    entry_points={},
+    entry_points={'console_scripts': [
+            'fordead = fordead.cli.__main__:cli',
+        ],},
 )
