2e189074de4c8497dec1734a8b1ce72f29906575
Raphael Dutrieux <raphael.dutrieux@inrae.fr>
2021-02-15 18:16:32 +0100

    steps instead of cli in setup

--- setup.py
+++ setup.py
@@ -153,6 +153,6 @@ setup(
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
     entry_points={'console_scripts': [
-            'fordead = fordead.cli.__main__:fordead',
+            'fordead = fordead.steps.__main__:fordead',
         ],},
 )
