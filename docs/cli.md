1b4a39775abc1c0c0ccc2c0602da87bb03378c4a
Raphael Dutrieux <raphael.dutrieux@inrae.fr>
2021-03-03 16:54:31 +0100

    Test other cli group

--- docs/cli.md
+++ docs/cli.md
@@ -3,6 +3,11 @@
 This page provides documentation for our command line tools.
 
 ::: mkdocs-click
-    :module: fordead.__main__
+    :module: fordead.steps.__main__
     :command: fordead
-    :depth: 2
+    :depth: 1
+	
+::: mkdocs-click
+    :module: fordead.cli.cli_read_tileinfo
+    :command: cli
+    :depth: 1
--- fordead/cli/cli_read_tileinfo.py
+++ fordead/cli/cli_read_tileinfo.py
@@ -1,7 +1,13 @@
 import click
 from fordead.ImportData import TileInfo
 
-@click.command(name='read_tileinfo')
+import click
+
+@click.group()
+def cli():
+    """Main entrypoint."""
+
+@cli.command(name='read_tileinfo')
 @click.option("-o", "--data_directory", type = str, help = "Path of the output directory",default =  "/mnt/fordead/Data/SENTINEL/")
 def cli_read_tileinfo(data_directory):
     """
