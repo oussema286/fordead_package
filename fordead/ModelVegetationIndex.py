6fd8184d6fe42c1209eb831cbbddeab33510c204
Raphael Dutrieux <raphael.dutrieux@inrae.fr>
2021-03-18 15:50:30 +0100

    test

--- fordead/ModelVegetationIndex.py
+++ fordead/ModelVegetationIndex.py
@@ -218,14 +218,14 @@ def prediction_vegetation_index(coeff_model,date_list):
 
 def model_vi_correction(stack_vi, mask_path):
     forest_mask = import_forest_mask(mask_path, chunks = 1280)
-    stack_vi = stack_vi.chunk({"Time": 1,"x" : -1,"y" : -1})
-    median_vi = stack_vi.where(forest_mask).median(dim=["x","y"],skipna=True).load(scheduler="single-threaded")
-    print("median computed")
+    stack_vi = stack_vi.chunk({"Time": 1,"x" : 1280,"y" : 1280})
+    median_vi = stack_vi.where(forest_mask).median(dim=["x","y"],skipna=True)
+    # print("median computed")
     stack_vi = stack_vi.chunk({"Time": -1,"x" : 1280,"y" : 1280})
-    # large_scale_model = model_vi(median_vi, xr.DataArray(np.zeros((median_vi.size),dtype = bool), coords=median_vi.coords), one_dim = True)
-    # predicted_median_vi = prediction_vegetation_index(large_scale_model,median_vi.Time.data)
-    # correction_vi = (predicted_median_vi - median_vi)
-    # stack_vi = stack_vi + correction_vi
+    large_scale_model = model_vi(median_vi, xr.DataArray(np.zeros((median_vi.size),dtype = bool), coords=median_vi.coords), one_dim = True)
+    predicted_median_vi = prediction_vegetation_index(large_scale_model,median_vi.Time.data)
+    correction_vi = (predicted_median_vi - median_vi)
+    stack_vi = stack_vi + correction_vi
     # stack_vi = stack_vi + xr.DataArray(np.ones((stack_vi.sizes["Time"]),dtype = int),coords = stack_vi.Time.coords)
     # , large_scale_model, correction_vi
     return stack_vi, 1, 1
