91ee84b74ab958501488a087d93ccd2b83776e93
Raphael Dutrieux <raphael.dutrieux@inrae.fr>
2021-04-06 16:46:47 +0100

    correct parameter

--- examples/tests_validation.py
+++ examples/tests_validation.py
@@ -70,7 +70,7 @@ for max_last_date_training in ["2018-06-01", "2018-08-01", "2018-10-01"]:
     process_tiles(main_directory = main_directory, sentinel_directory = sentinel_directory, tuiles = tuiles, forest_mask_source = "BDFORET", extent_shape_path = None,
                     dep_path = dep_path, bdforet_dirpath = bdforet_dirpath, list_forest_type =  ["FF2-00-00", "FF2-90-90", "FF2-91-91", "FF2G61-61"], path_oso = None, list_code_oso = None, #compute_forest_mask arguments
                     lim_perc_cloud = 0.4, vi = vi, sentinel_source = "THEIA", apply_source_mask = False, #compute_masked_vegetationindex arguments
-                    min_last_date_training = "2018-01-01", max_last_date_training = "2018-08-01", nb_min_date = 11,#Train_model arguments
+                    min_last_date_training = "2018-01-01", max_last_date_training = max_last_date_training, nb_min_date = 11,#Train_model arguments
                     threshold_anomaly = threshold_anomaly,
                     start_date_results = '2015-06-23', end_date_results = '2022-06-23', results_frequency = "M", multiple_files = False,
                     correct_vi = correct_vi, validation = True)
