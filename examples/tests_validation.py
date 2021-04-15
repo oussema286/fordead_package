138981d000b425ab3fb29da1d1973960a3df90a8
Raphael Dutrieux <raphael.dutrieux@inrae.fr>
2021-04-15 17:52:27 +0100

    tests T31UGP

--- examples/tests_validation.py
+++ examples/tests_validation.py
@@ -11,9 +11,10 @@ from examples.copy_validation_data import copy_validation_data
 import shutil
 import os
 from pathlib import Path
+from fordead.visualisation.create_timelapse import create_timelapse
+from fordead.visualisation.vi_series_visualisation import vi_series_visualisation
 
 dep_path = "/mnt/fordead/Data/Vecteurs/Departements/departements-20140306-100m.shp"
-tuiles = ["T31UFQ", "T31UFR", "T31UGP", "T31UGQ", "T31TGL"]
 sentinel_directory = "/mnt/fordead/Data/SENTINEL/"
 bdforet_dirpath = "/mnt/fordead/Data/Vecteurs/BDFORET"
 
@@ -79,10 +80,80 @@ bdforet_dirpath = "/mnt/fordead/Data/Vecteurs/BDFORET"
 #         for tile in tuiles:
 #             shutil.copytree(main_directory+"/"+tile+"/Results", "/mnt/fordead/Out/"+name_dir+"/"+tile)
             
+
+            
+
+vi = "CRSWIR" ; threshold_anomaly = 0.16 ; correct_vi = False
+tuiles = ["T31UGP"]
+if correct_vi:
+    name_dir = vi+"_"+str(threshold_anomaly)+"_corrected"
+else:
+    name_dir = vi+"_"+str(threshold_anomaly)
+process_tiles(main_directory = name_dir, sentinel_directory = sentinel_directory, tuiles = tuiles, forest_mask_source = "BDFORET", extent_shape_path = None,
+                dep_path = dep_path, bdforet_dirpath = bdforet_dirpath, list_forest_type =  ["FF2-00-00", "FF2-90-90", "FF2-91-91", "FF2G61-61"], path_oso = None, list_code_oso = None, #compute_forest_mask arguments
+                lim_perc_cloud = 0.4, vi = vi, sentinel_source = "THEIA", apply_source_mask = False, #compute_masked_vegetationindex arguments
+                min_last_date_training = "2018-01-01", max_last_date_training = "2018-08-01", nb_min_date = 11,#Train_model arguments
+                threshold_anomaly = threshold_anomaly,
+                start_date_results = '2015-06-23', end_date_results = '2022-06-23', results_frequency = "M", multiple_files = False,
+                correct_vi = correct_vi, validation = True)
+create_timelapse(data_directory = Path(name_dir) / tuiles[0] , x = 803986, y= 5346917, buffer = 1000)
+vi_series_visualisation(data_directory = Path(name_dir) / tuiles[0], ymin = 0, ymax = 2,chunks = 1280, shape_path = "/mnt/fordead/Data/Vecteurs/points_visualisation.shp")
+
+
+vi = "CRSWIR" ; threshold_anomaly = 0.14 ; correct_vi = True
+if correct_vi:
+    name_dir = vi+"_"+str(threshold_anomaly)+"_corrected"
+else:
+    name_dir = vi+"_"+str(threshold_anomaly)
+process_tiles(main_directory = name_dir, sentinel_directory = sentinel_directory, tuiles = tuiles, forest_mask_source = "BDFORET", extent_shape_path = None,
+                dep_path = dep_path, bdforet_dirpath = bdforet_dirpath, list_forest_type =  ["FF2-00-00", "FF2-90-90", "FF2-91-91", "FF2G61-61"], path_oso = None, list_code_oso = None, #compute_forest_mask arguments
+                lim_perc_cloud = 0.4, vi = vi, sentinel_source = "THEIA", apply_source_mask = False, #compute_masked_vegetationindex arguments
+                min_last_date_training = "2018-01-01", max_last_date_training = "2018-08-01", nb_min_date = 11,#Train_model arguments
+                threshold_anomaly = threshold_anomaly,
+                start_date_results = '2015-06-23', end_date_results = '2022-06-23', results_frequency = "M", multiple_files = False,
+                correct_vi = correct_vi, validation = True)
+create_timelapse(data_directory = Path(name_dir) / tuiles[0] , x = 803986, y= 5346917, buffer = 1000)
+vi_series_visualisation(data_directory = Path(name_dir) / tuiles[0], ymin = 0, ymax = 2,chunks = 1280, shape_path = "/mnt/fordead/Data/Vecteurs/points_visualisation.shp")
+
+
+vi = "NDWI" ; threshold_anomaly = 0.14 ; correct_vi = False
+if correct_vi:
+    name_dir = vi+"_"+str(threshold_anomaly)+"_corrected"
+else:
+    name_dir = vi+"_"+str(threshold_anomaly)
+process_tiles(main_directory = name_dir, sentinel_directory = sentinel_directory, tuiles = tuiles, forest_mask_source = "BDFORET", extent_shape_path = None,
+                dep_path = dep_path, bdforet_dirpath = bdforet_dirpath, list_forest_type =  ["FF2-00-00", "FF2-90-90", "FF2-91-91", "FF2G61-61"], path_oso = None, list_code_oso = None, #compute_forest_mask arguments
+                lim_perc_cloud = 0.4, vi = vi, sentinel_source = "THEIA", apply_source_mask = False, #compute_masked_vegetationindex arguments
+                min_last_date_training = "2018-01-01", max_last_date_training = "2018-08-01", nb_min_date = 11,#Train_model arguments
+                threshold_anomaly = threshold_anomaly,
+                start_date_results = '2015-06-23', end_date_results = '2022-06-23', results_frequency = "M", multiple_files = False,
+                correct_vi = correct_vi, validation = True)
+create_timelapse(data_directory = Path(name_dir) / tuiles[0] , x = 803986, y= 5346917, buffer = 1000)
+vi_series_visualisation(data_directory = Path(name_dir) / tuiles[0], ymin = 0, ymax = 2,chunks = 1280, shape_path = "/mnt/fordead/Data/Vecteurs/points_visualisation.shp")
+
+vi = "NDWI" ; threshold_anomaly = 0.12 ; correct_vi = True
+if correct_vi:
+    name_dir = vi+"_"+str(threshold_anomaly)+"_corrected"
+else:
+    name_dir = vi+"_"+str(threshold_anomaly)
+process_tiles(main_directory = name_dir, sentinel_directory = sentinel_directory, tuiles = tuiles, forest_mask_source = "BDFORET", extent_shape_path = None,
+                dep_path = dep_path, bdforet_dirpath = bdforet_dirpath, list_forest_type =  ["FF2-00-00", "FF2-90-90", "FF2-91-91", "FF2G61-61"], path_oso = None, list_code_oso = None, #compute_forest_mask arguments
+                lim_perc_cloud = 0.4, vi = vi, sentinel_source = "THEIA", apply_source_mask = False, #compute_masked_vegetationindex arguments
+                min_last_date_training = "2018-01-01", max_last_date_training = "2018-08-01", nb_min_date = 11,#Train_model arguments
+                threshold_anomaly = threshold_anomaly,
+                start_date_results = '2015-06-23', end_date_results = '2022-06-23', results_frequency = "M", multiple_files = False,
+                correct_vi = correct_vi, validation = True)
+create_timelapse(data_directory = Path(name_dir) / tuiles[0] , x = 803986, y= 5346917, buffer = 1000)
+vi_series_visualisation(data_directory = Path(name_dir) / tuiles[0], ymin = 0, ymax = 2,chunks = 1280, shape_path = "/mnt/fordead/Data/Vecteurs/points_visualisation.shp")
+
+
+
+
 correct_vi = False
 vi = "CRSWIR"
 threshold_anomaly = 0.16
-for nb_min_date in [10,12,13]:
+tuiles = ["T31UFQ", "T31UFR", "T31UGP", "T31UGQ", "T31TGL"]
+for nb_min_date in [12,13]:
     for max_last_date_training in ["2018-08-01"]:
         main_directory = "/mnt/fordead/Out/"+vi
         name_dir = vi+"_"+max_last_date_training+"_"+str(nb_min_date)
