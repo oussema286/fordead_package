e1736ca0d8fc8e0893bfb726ffb0bb3a3b777c68
Florian de Boissieu <fdeboiss@gmail.com>
2024-04-23 01:05:07 +0200

    fix CLI doc and move the forest mask to step 4 to be coherent with user guides

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -12,34 +12,39 @@ def test_fordead_steps(input_dir):
         test_output_dir = Path(tempdir)
         data_directory = test_output_dir / "dieback_detection"
 
-        compute_masked_vegetationindex(input_directory = input_dir / "sentinel_data" / "dieback_detection_tutorial" / "study_area", 
-                                    data_directory = data_directory, 
-                                    lim_perc_cloud = 0.4, 
-                                    interpolation_order = 0, 
-                                    sentinel_source  = "THEIA", 
-                                    soil_detection = False, 
-                                    formula_mask = "B2 > 600", 
-                                    vi = "CRSWIR", 
-                                    apply_source_mask = True)
+        compute_masked_vegetationindex(
+            input_directory = input_dir / "sentinel_data" / "dieback_detection_tutorial" / "study_area", 
+            data_directory = data_directory, 
+            lim_perc_cloud = 0.4, 
+            interpolation_order = 0, 
+            sentinel_source  = "THEIA", 
+            soil_detection = False, 
+            formula_mask = "B2 > 600", 
+            vi = "CRSWIR", 
+            apply_source_mask = True)
 
-        train_model(data_directory = data_directory, 
-                    nb_min_date = 10, 
-                    min_last_date_training="2018-01-01", 
-                    max_last_date_training="2018-06-01")
+        train_model(
+            data_directory = data_directory, 
+            nb_min_date = 10, 
+            min_last_date_training="2018-01-01", 
+            max_last_date_training="2018-06-01")
 
-        dieback_detection(data_directory = data_directory, 
-                        threshold_anomaly = 0.16,
-                        stress_index_mode = "weighted_mean")
+        dieback_detection(
+            data_directory = data_directory, 
+            threshold_anomaly = 0.16,
+            stress_index_mode = "weighted_mean")
 
-        compute_forest_mask(data_directory, 
-                            forest_mask_source = "vector", 
-                            vector_path = input_dir / "vector" / "area_interest.shp")
+        compute_forest_mask(
+            data_directory, 
+            forest_mask_source = "vector", 
+            vector_path = input_dir / "vector" / "area_interest.shp")
 
-        export_results(data_directory = data_directory, 
-                    frequency= "ME", 
-                    multiple_files = False, 
-                    conf_threshold_list = [0.265],
-                    conf_classes_list = ["Low anomaly","Severe anomaly"])
+        export_results(
+            data_directory = data_directory, 
+            frequency= "ME", 
+            multiple_files = False, 
+            conf_threshold_list = [0.265],
+            conf_classes_list = ["Low anomaly","Severe anomaly"])
 
 def test_process_tiles(input_dir):
     with TemporaryDirectory(prefix="fordead-tests_") as tempdir:
