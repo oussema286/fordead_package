706bf08aa5f75d704a6f990432cae1a33bc7a473
Florian de Boissieu <fdeboiss@gmail.com>
2024-11-23 17:42:24 +0100

    fix ci script

--- ci/prepare_dinamis_sdk.py
+++ ci/prepare_dinamis_sdk.py
@@ -8,7 +8,7 @@ if not DINAMIS_API_KEY_FILE.exists():
     DINAMIS_API_KEY = os.getenv("DINAMIS_API_KEY")
     DINAMIS_API_SECRET = os.getenv("DINAMIS_API_SECRET")
 
-    DINAMIS_API_KEY_FILE.parent.mkdir_p()
+    DINAMIS_API_KEY_FILE.parent.makedirs_p()
 
     api_key = {"access-key": DINAMIS_API_KEY, "secret-key": DINAMIS_API_SECRET}
     with open(DINAMIS_API_KEY_FILE, "w") as f:
