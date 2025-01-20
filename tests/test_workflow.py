23e589ef8c71f1b311411669dc1f2966d3105449
Florian de Boissieu <fdeboiss@gmail.com>
2025-01-20 22:38:11 +0000

    Resolve "add warning if version of processed scenes different then local files"

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -1,6 +1,7 @@
 import geopandas as gpd
 from path import Path
 from tempfile import TemporaryDirectory
+import re
 from fordead.import_data import TileInfo
 from fordead.steps.step1_compute_masked_vegetationindex import compute_masked_vegetationindex
 from fordead.steps.step2_train_model import train_model
@@ -9,6 +10,51 @@ from fordead.steps.step4_compute_forest_mask import compute_forest_mask
 from fordead.steps.step5_export_results import export_results, extract_results
 from fordead.cli.cli_process_tiles import process_tiles
 from fordead.visualisation.vi_series_visualisation import vi_series_visualisation
+
+# should generate a warning
+def test_warning_if_version_changed(input_dir, output_dir):
+    test_output_dir = (output_dir / "workflow_steps").rmtree_p().mkdir()
+    data_directory = test_output_dir / "dieback_detection"
+    start_date = "2016-01-01"
+    sentinel_dir = input_dir / "sentinel_data" / "dieback_detection_tutorial" / "study_area"
+
+    compute_masked_vegetationindex(
+        input_directory = sentinel_dir, 
+        data_directory = data_directory,
+        start_date=start_date,
+        lim_perc_cloud = 0.4, 
+        interpolation_order = 0, 
+        sentinel_source  = "theia", 
+        soil_detection = False, 
+        formula_mask = "B2 > 600", 
+        vi = "CRSWIR", 
+        apply_source_mask = True)
+
+    tile = TileInfo(data_directory)
+    tile = tile.import_info()
+    scene = list(list(tile.paths["Sentinel"].values())[0].values())[0].parent
+
+    sentinel_dir2 = (sentinel_dir+"_warning").rmtree_p()
+    sentinel_dir.copytree(sentinel_dir2)
+    
+    scene = sentinel_dir2 / scene.name
+    # simulate upgrade
+    new_scene = re.sub("_V[0-9]-[0-9]$", "_V5-0", scene)
+    scene.move(new_scene)
+
+    # should generate a warning
+    compute_masked_vegetationindex(
+        input_directory = sentinel_dir2, 
+        data_directory = data_directory,
+        start_date=start_date,
+        lim_perc_cloud = 0.4, 
+        interpolation_order = 0, 
+        sentinel_source  = "theia", 
+        soil_detection = False, 
+        formula_mask = "B2 > 600", 
+        vi = "CRSWIR", 
+        apply_source_mask = True)
+
 
 def test_fordead_steps(input_dir, output_dir):
    
